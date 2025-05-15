1c49aa05bc3afd73e8d6d332f497493ac0395f0e
H30312 <H30312@DESKTOP-GFN5PM7>
2025-05-15 11:18:23 +0900

    接続テスト

--- src/main/java/model/LoginCheck.java
+++ src/main/java/model/LoginCheck.java
@@ -6,7 +6,7 @@ import dao.UserDAO;
 public class LoginCheck {
 	public boolean execute(User user) {
 		UserDAO dao = new UserDAO();
-		boolean a = dao.checkAccount(user);
+		boolean a = dao.findUser(user);
 		return a;
 
 	}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,32 +1,32 @@
 package model;
 
 public class User {
-    private int id;
-    private String username;
+    private int userId;
+    private String userName;
     private String password; 
     
     // デフォルトコンストラクタ
     public User() {}
     // 引数アリのコンストラクタ
     public User(String username , String password) {
-        this.username = username;
+        this.userName = username;
         this.password = password;
     }
 
-    public int getId() {
-    	return id; 
+    public int getUserId() {
+    	return userId;
     }
     
-    public void setId(int id) {
-    	this.id = id; 
+    public void setUserId(int id) {
+    	this.userId = userId; 
     }
     
     public String getUsername() {
-    	return username; 
+    	return userName; 
     }
     
     public void setUsername(String username) {
-    	this.username = username; 
+    	this.userName = username; 
     }
     
     public String getPassword() {
